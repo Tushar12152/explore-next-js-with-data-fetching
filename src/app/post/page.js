b6f927c0b91d2971b7bcbbd9cde831a12c4a158b
Tushar <tusharimran470@gmail.com>
2024-06-28 10:20:34 +0600

    ssg/ssr concept added

--- src/app/post/page.js
+++ src/app/post/page.js
@@ -1,15 +1,29 @@
 
 const PostPage = async () => {
 
-    const res = await fetch('http://localhost:8005/posts',{
-        cache:'force-cache'
-    });
+    const res = await fetch('http://localhost:8005/posts',
+        // { cache:'force-cache'}
+
+
+
+        //for e-comerce website
+        // {
+        //     next:{
+        //         revalidate:1,
+        //     }
+        // }
+
+
+        //for news portal-social media type website.
+        {cache:"no-store"}
+    
+    );
     const posts = await res.json();
     // console.log(posts)
 
     return (
         <div>
-            <h1 className="text-4xl text-center font-bold">Total post: {posts?.length}</h1>
+            <h1 className="text-4xl text-center font-bold">Total posted: {posts?.length}</h1>
 
             <div className="w-[1200px] mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10 gap-6">
                 {
